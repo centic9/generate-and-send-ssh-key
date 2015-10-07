b37b4bc9c55f8fd069e0b35f61d6daa8898e97f7
Dominik Stadler <dominik.stadler@gmx.at>
2015-10-07 12:44:21 +0200

    Adjust checks, add permissions of local files, add more output

--- generate-key-and-send.sh
+++ generate-key-and-send.sh
@@ -13,33 +13,45 @@ USER=username
 #
 
 # check that we have all necessary parts
-wich ssh-keygen && which ssh-copy-id && which ssh
-RET=$?
-if [ $RET -ne 0 ];then
-    echo Could not find the required tools, needed are 'ssh', 'ssh-keygen', 'ssh-copy-id': $RET
+SSH_KEYGEN=`which ssh-keygen`
+SSH=`which ssh`
+SSH_COPY_ID=`which ssh-copy-id`
+if [ -z "$SSH_KEYGEN" -o -z "$SSH" -o -z "$SSH_COPY_ID" ];then
+    echo Could not find the required tools, we need 'ssh', 'ssh-keygen', 'ssh-copy-id': $RET
     exit 1
 fi
 
 # perform the actual work
-ssh-keygen -t $KEYTYPE -b $KEYSIZE  -f $FILENAME -N "$PASSPHRASE"
+echo Creating a new key using $SSH-KEYGEN
+$SSH_KEYGEN -t $KEYTYPE -b $KEYSIZE  -f $FILENAME -N "$PASSPHRASE"
 RET=$?
 if [ $RET -ne 0 ];then
     echo ssh-keygen failed: $RET
     exit 1
 fi
 
-ssh-copy-id -i $FILENAME $USER@$HOST
+echo Adjust permissions of generated key-files locally
+chmod 0700 ${FILENAME}*
+RET=$?
+if [ $RET -ne 0 ];then
+    echo chmod failed: $RET
+    exit 1
+fi
+
+echo Copying the key to the remote machine $USER@$HOST
+$SSH_COPY_ID -i $FILENAME $USER@$HOST
 RET=$?
 if [ $RET -ne 0 ];then
     echo ssh-copy-id failed: $RET
     exit 1
 fi
 
-ssh $USER@$HOST "chmod go-w ~ && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys"
+echo Adjusting permissions to avoid errors in ssh-daemon
+$SSH $USER@$HOST "chmod go-w ~ && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys"
 RET=$?
 if [ $RET -ne 0 ];then
     echo ssh-chmod failed: $RET
     exit 1
 fi
 
-echo Setup finished, now try to run ssh -i $FILENAME $USER/$HOST
+echo Setup finished, now try to run ssh -i $FILENAME $USER@$HOST
