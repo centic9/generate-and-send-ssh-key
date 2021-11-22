04bfbafcc99e2e80d18a6aa69734542434e07052
Dominik Stadler <dominik.stadler@gmx.at>
2021-11-22 16:22:15 +0100

    Use curly-braces and quote filenames

--- generate-and-send-ssh-key.sh
+++ generate-and-send-ssh-key.sh
@@ -21,11 +21,11 @@ function usage() {
     echo "  -u (--user)       <username>, default: ${USER}"
     echo "  -f (--file)       <file>,     default: ${FILENAME}"
     echo "  -h (--host)       <hostname>, default: ${HOST}"
-    
+
     echo "  -p (--port)       <port>,     default: <default ssh port>"
     echo "  -k (--keysize)    <size>,     default: ${KEYSIZE}"
     echo "  -t (--keytype)    <type>,     default: ${KEYTYPE}"
-    
+
     echo "  -P (--passphrase) <key-passphrase>, default: ${PASSPHRASE}"
 
     exit 2
@@ -86,82 +86,82 @@ SSH_KEYGEN=`which ssh-keygen`
 SSH=`which ssh`
 SSH_COPY_ID=`which ssh-copy-id`
 
-if [ -z "$SSH_KEYGEN" ];then
+if [ -z "${SSH_KEYGEN}" ];then
     echo Could not find the 'ssh-keygen' executable
     exit 1
 fi
-if [ -z "$SSH" ];then
+if [ -z "${SSH}" ];then
     echo Could not find the 'ssh' executable
     exit 1
 fi
 
 echo
 # perform the actual work
-if [ -f $FILENAME ];then
+if [ -f "${FILENAME}" ];then
     echo Using existing key
 else
-    echo Creating a new key using $SSH-KEYGEN
-    $SSH_KEYGEN -t $KEYTYPE -b $KEYSIZE  -f $FILENAME -N "$PASSPHRASE"
+    echo Creating a new key using ${SSH-KEYGEN}
+    ${SSH_KEYGEN} -t $KEYTYPE -b $KEYSIZE  -f "${FILENAME}" -N "${PASSPHRASE}"
     RET=$?
-    if [ $RET -ne 0 ];then
-        echo ssh-keygen failed: $RET
+    if [ ${RET} -ne 0 ];then
+        echo ssh-keygen failed: ${RET}
         exit 1
     fi
 fi
 
-if [ ! -f ${FILENAME}.pub ];then
+if [ ! -f "${FILENAME}.pub" ];then
     echo Did not find the expected public key at ${FILENAME}.pub
     exit 1
 fi
 
 echo
 echo Having key-information
-ssh-keygen -l -f ${FILENAME}
+ssh-keygen -l -f "${FILENAME}"
 
 echo
 echo Adjust permissions of generated key-files locally
-chmod 0600 ${FILENAME} ${FILENAME}.pub
+chmod 0600 "${FILENAME}" "${FILENAME}.pub"
 RET=$?
-if [ $RET -ne 0 ];then
-    echo chmod failed: $RET
+if [ ${RET} -ne 0 ];then
+    echo chmod failed: ${RET}
     exit 1
 fi
 
 echo
-echo Copying the key to the remote machine $USER@$HOST, this should ask for the password
-if [ -z "$SSH_COPY_ID" ];then
+echo Copying the key to the remote machine ${USER}@${HOST}, this should ask for the password
+if [ -z "${SSH_COPY_ID}" ];then
     echo Could not find the 'ssh-copy-id' executable, using manual copy instead
-    cat ${FILENAME}.pub | ssh $SSH_OPTS $USER@$HOST 'cat >> ~/.ssh/authorized_keys'
+    cat "${FILENAME}.pub" | ssh ${SSH_OPTS} ${USER}@${HOST} 'cat >> ~/.ssh/authorized_keys'
 else
-    $SSH_COPY_ID $SSH_OPTS -i $FILENAME.pub $USER@$HOST
+    ${SSH_COPY_ID} ${SSH_OPTS} -i ${FILENAME}.pub ${USER}@${HOST}
     RET=$?
-    if [ $RET -ne 0 ];then
-      echo Executing ssh-copy-id via $SSH_COPY_ID failed, trying to manually copy the key-file instead
-      cat ${FILENAME}.pub | ssh $SSH_OPTS $USER@$HOST 'cat >> ~/.ssh/authorized_keys'
+    if [ ${RET} -ne 0 ];then
+      echo Executing ssh-copy-id via ${SSH_COPY_ID} failed, trying to manually copy the key-file instead
+      cat "${FILENAME}.pub" | ssh ${SSH_OPTS} ${USER}@${HOST} 'cat >> ~/.ssh/authorized_keys'
     fi
 fi
 
 RET=$?
-if [ $RET -ne 0 ];then
-    echo ssh-copy-id failed: $RET
+if [ ${RET} -ne 0 ];then
+    echo ssh-copy-id failed: ${RET}
     exit 1
 fi
 
 echo
 echo Adjusting permissions to avoid errors in ssh-daemon, this will ask once more for the password
-$SSH $SSH_OPTS $USER@$HOST "chmod go-w ~ && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys"
+${SSH} ${SSH_OPTS} ${USER}@${HOST} "chmod go-w ~ && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys"
 RET=$?
-if [ $RET -ne 0 ];then
-    echo ssh-chmod failed: $RET
+if [ ${RET} -ne 0 ];then
+    echo ssh-chmod failed: ${RET}
     exit 1
 fi
 
 # Cut out PubKeyAuth=no here as it should work without it now
 echo
-echo Setup finished, now try to run $SSH `echo $SSH_OPTS | sed -e 's/-o PubkeyAuthentication=no//g'` -i $FILENAME $USER@$HOST
+echo Setup finished, now try to run ${SSH} `echo ${SSH_OPTS} | sed -e 's/-o PubkeyAuthentication=no//g'` -i "${FILENAME}" ${USER}@${HOST}
 
 echo
 echo If it still does not work, you can try the following steps:
-echo "- Make sure ~/.ssh/config does not have custom configuration for this host"
+echo "- Check if ~/.ssh/config has some custom configuration for this host"
 echo "- Make sure the type of key is supported, e.g. 'dsa' is deprecated and might be disabled"
 echo "- Try running ssh with '-v' and look for clues in the resulting output"
